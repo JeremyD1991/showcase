874998ec8fc83abe37e2bedc2714513b45de7189
Jeremy <jdwilliams1991@gmail.com>
2017-12-01 11:20:09 -0500

    Introducing functionality

--- resources/js/jquery.gallery.js
+++ resources/js/jquery.gallery.js
@@ -1,3 +1,9 @@
 $( document ).ready(function() {
-    jQuery('a.gallery').colorbox({rel:'group1'});
+    jQuery('a.gallery').colorbox({rel:'group1', transition:"elastic"});
+	$(".lazy").slick({
+        lazyLoad: 'ondemand', // ondemand progressive anticipated
+        infinite: true,
+		autoplay: true,
+		autoplaySpeed: 3000
+      });
 });
