351e49065a51efb97e66ef6409c8ac2ac677761d
Jeremy <jdwilliams1991@gmail.com>
2017-12-13 16:36:16 -0500

    mobile pass

--- resources/js/jquery.gallery.js
+++ resources/js/jquery.gallery.js
@@ -1,9 +1,12 @@
 $( document ).ready(function() {
-    jQuery('a.gallery').colorbox({rel:'group1', transition:"elastic"});
-	$(".lazy").slick({
+    jQuery('a.gallery').colorbox({rel:'group1', transition:'elastic'});
+	$('.lazy').slick({
         lazyLoad: 'ondemand', // ondemand progressive anticipated
         infinite: true,
 		autoplay: true,
 		autoplaySpeed: 3000
       });
+	$('.toggle').onclick(function() {
+		$('.sub').toggleClass('active');
+	});
 });
